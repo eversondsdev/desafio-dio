519fb1386f57afc03c2aa280241ef8bb84b8099e
Everson Daniel <eversondaniel98@gmail.com>
2023-12-08 12:36:29 -0300

    funcao classificador de heroi

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Aprendendo Git e Github")
+
+function Heroi(nome, xp){
+  let level = " "
+  if(xp <= 1000) level = "Ferro"
+  else if( xp > 1000 && xp <= 2000) level = "Bronze"
+  else if(xp > 2000 && xp <= 5000) level = "Prata"
+  else if(xp > 5000 && xp <= 7000) level = "Ouro"
+  else if(xp > 7000 && xp <= 8000) level = "Platina"
+  else if(xp > 8000 && xp <= 9000) level = "Ascendente"
+  else if(xp > 9000 && xp <= 10000) level = "Imortal"
+  else if(xp > 10000) level = "Radiante"
+  else console.log("O Heroi não pode ter XP menor que 0")
+
+  console.log(`O Heroi ${nome}, está no nível ${level}.`)
+}
+Heroi('Daniel', 8001);
